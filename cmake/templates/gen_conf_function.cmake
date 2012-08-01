70532c2f3b075619dae64215fdda74e628856da5
Jostein Bratlie <jostein.bratlie@gmail.com>
2012-08-01 12:13:05 +0200

    On build: now creates a clean build directory, build, both on in and out of source builds. Links all headers back to the source directory. On deployment/installation headers/template sources should be copied. See CPack.

--- cmake/templates/gen_conf_function.cmake
+++ cmake/templates/gen_conf_function.cmake
@@ -6,20 +6,22 @@ foreach( HDR_SET ${HEADERS} )
   list( GET ${HDR_SET} 1 GEN_INCLUDE )
   list( GET ${HDR_SET} 2 GEN_FILE )
 
-  set( VAR_HEADER_INCLUDES ${GEN_INCLUDE}/${GEN_FILE} )
+
+  file( RELATIVE_PATH INC_PATH ${GEN_DIR} ${GEN_INCLUDE} )
+  set( VAR_HEADER_INCLUDES "#include \"${INC_PATH}/${GEN_FILE}\"" )
 
   message( "------------------------------------------------------------" )
   message( "TEMPLATE LOC:    ${TEMPLATE_LOC}" )
   message( "DST Path:        ${GEN_DIR}/${GEN_FILE}" )
   message( "INC:             ${VAR_HEADER_INCLUDES}" )
+  message( "INC Path:        ${INC_PATH}" )
   message( "------------------------------------------------------------" )
 
-
-#  execute_process( COMMAND ${CMAKE_COMMAND} -E make_directory ${GEN_DIR} )
-#  configure_file(
-#    ${TEMPLATE_LOC}
-#    ${GEN_DIR}/${GEN_FILE}
-#    @ONLY
-#  )
+  execute_process( COMMAND ${CMAKE_COMMAND} -E make_directory ${GEN_DIR} )
+  configure_file(
+    ${TEMPLATE_LOC}
+    ${GEN_DIR}/${GEN_FILE}
+    @ONLY
+  )
 
 endforeach()
