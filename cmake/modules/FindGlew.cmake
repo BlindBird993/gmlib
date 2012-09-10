59aca2538eecc82b2309b5b70b07ce557188a3c0
Jostein Bratlie <jostein.bratlie@gmail.com>
2012-09-10 23:43:59 +0200

    Build system update to suport building with MSVC2010. ECMake sync.

--- cmake/modules/FindGlew.cmake
+++ cmake/modules/FindGlew.cmake
@@ -7,16 +7,25 @@
 # GLEW_LIBRARY
 # 
 
+
+set( GLEW_PATH_WIN32 $ENV{PROGRAMFILES}/glew CACHE PATH "Custom GLEW PATH." )
+#if( NOT CUSTOM_GLEW_PATH_WIN32 )
+#  set( GLEW_PATH $ENV{PROGRAMFILES}/glew )
+#else()
+#  set( GLEW_PATH ${CUSTOM_GLEW_PATH_WIN32} )
+#endif()
+
+
 IF (WIN32)
   FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
-    $ENV{PROGRAMFILES}/GLEW/include
+    ${GLEW_PATH_WIN32}/include
     DOC "The directory where GL/glew.h resides"
     NO_DEFAULT_PATH
     )
   FIND_LIBRARY( GLEW_LIBRARY
     NAMES glew GLEW glew32 glew32s
     PATHS
-    $ENV{PROGRAMFILES}/GLEW/lib
+    ${GLEW_PATH_WIN32}/lib
     DOC "The GLEW library"
     NO_DEFAULT_PATH
     )
