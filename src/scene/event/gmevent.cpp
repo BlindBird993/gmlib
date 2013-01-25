9cc5d3a2b8ef47152b2fb826cbbe14c324e49343
Rune Dalmo <rune.dalmo@gmail.com>
2013-01-25 11:52:12 +0100

    Implementation of inline funcs moved to .cpp file

--- src/scene/event/gmevent.cpp
+++ src/scene/event/gmevent.cpp
@@ -5,21 +5,3 @@ using namespace GMlib;
 Event::Event(double x) :
   _x(x) {
 }
-
-inline
-double
-Event::getX() const {
-  return _x;
-}
-
-inline
-bool
-Event::operator <(const Event &v) const {
-  return _x < v._x;
-}
-
-inline
-bool
-Event::operator ==(const Event &v) const {
-  return _x == v._x;
-}
