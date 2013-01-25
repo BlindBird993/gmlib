21988ba171f56d9933b7164f32bac66615fa0eb8
Rune Dalmo <rune.dalmo@gmail.com>
2013-01-25 12:04:41 +0100

    Implementation of inline funcs moved to .h file

--- src/scene/event/gmevent.h
+++ src/scene/event/gmevent.h
@@ -45,6 +45,21 @@ namespace GMlib {
     double _x;
   };
 
+  inline double
+  Event::getX() const {
+    return _x;
+  }
+
+  inline bool
+  Event::operator <(const Event &v) const {
+    return _x < v._x;
+  }
+
+  inline bool
+  Event::operator ==(const Event &v) const {
+    return _x == v._x;
+  }
+
 }
 
 #endif
