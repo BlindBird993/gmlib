4d70bf2667150b7dc548d09401fb2be2af445871
Jostein Bratlie <jostein.bratlie@gmail.com>
2013-02-05 22:24:37 +0100

    Added some template utility functions in core/utils: ref. Stroustrup.

--- src/core/utils/gmutils.h
+++ src/core/utils/gmutils.h
@@ -59,6 +59,36 @@ namespace GMlib {
     }
 
 
+
+    //! \brief http://www.stroustrup.com/bs_faq2.html#contraints
+    template<class T, class B>
+    struct DerivedFrom {
+      static void constraints(T* p) { B* pb = p; }
+      DerivedFrom() { void(*p)(T*) = constraints; }
+    };
+
+    //! \brief http://www.stroustrup.com/bs_faq2.html#contraints
+    template<class T1, class T2>
+    struct CanCopy {
+      static void constraints(T1 a, T2 b) { T2 c = a; b = a; }
+      CanCopy() { void(*p)(T1,T2) = constraints; }
+    };
+
+    //! \brief http://www.stroustrup.com/bs_faq2.html#contraints
+    template<class T1, class T2 = T1>
+    struct CanCompare {
+      static void constraints(T1 a, T2 b) { a==b; a!=b; a<b; }
+      CanCompare() { void(*p)(T1,T2) = constraints; }
+    };
+
+    //! \brief http://www.stroustrup.com/bs_faq2.html#contraints
+    template<class T1, class T2, class T3 = T1>
+    struct CanMultiply {
+      static void constraints(T1 a, T2 b, T3 c) { c = a*b; }
+      CanMultiply() { void(*p)(T1,T2,T3) = constraints; }
+    };
+
+
   } // END namespace GMutils
 
 } // END namespace GMlib
