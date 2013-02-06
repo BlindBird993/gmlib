75eab78cf2a599f3b904439f52a6b720fec0ea6b
Rune Dalmo <rune.dalmo@gmail.com>
2013-02-06 10:55:54 +0100

    Flipping decompose and reconstruct filter coefficients calculation.

--- src/wavelet/gmorthofilter.c
+++ src/wavelet/gmorthofilter.c
@@ -11,12 +11,12 @@ template<typename T>
 void
 OrthoFilter<T>::calcFilters(const DVector<T> &coefs) {
   //- Normalize filter sum and store as decompose low-pass filter
-  this->_d_lp = coefs * ( T(1) / coefs.getSum() ) * sqrt(T(2));
+  this->_r_lp = coefs * ( T(1) / coefs.getSum() ) * sqrt(T(2));
 
   //- Compute hi-pass from low-pass
-  this->qmf(this->_d_lp, this->_d_hp);
+  this->qmf(this->_r_lp, this->_r_hp);
 
   //- Reconstruct filters are simply decompose filters reversed
-  this->_r_lp = this->_d_lp.getReversed();
-  this->_r_hp = this->_d_hp.getReversed();
+  this->_d_lp = this->_r_lp.getReversed();
+  this->_d_hp = this->_r_hp.getReversed();
 }
