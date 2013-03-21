c7e2456cce6a249488dcb64febbe30d0046ab40e
CMN <panser_bjarne@hotmail.com>
2013-03-21 15:47:30 +0100

    CMake script includes CuBLAS under Windows

--- cmake/modules/FindCuBLAS.cmake
+++ cmake/modules/FindCuBLAS.cmake
@@ -13,17 +13,37 @@ else(CMAKE_SIZEOF_VOID_P EQUAL 8)
   set(CUBLAS_64 "NO" CACHE BOOL "Use 64-bit CuBLAS implementation")
 endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
 option(CUBLAS_64 "64-bit CuBLAS")
-if(CUBLAS_64)
-  set(CUBLAS_64_SUFFIX "64")
-else(CUBLAS_64)
-  set(CUBLAS_64_SUFFIX "")
-endif(CUBLAS_64)
 
 IF (WIN32)
-#TODO
+  if(CUBLAS_64)
+    set(CUBLAS_64_PATH "x64")
+  else(CUBLAS_64)
+    set(CUBLAS_64_PATH "Win32")
+  endif(CUBLAS_64)
+  
+  set(CUDA_PATH $ENV{CUDA_PATH} CACHE PATH "Location of Cuda toolkit")
+  FIND_PATH( CUBLAS_INCLUDE_DIR cublas_v2.h
+    ${CUDA_PATH}/include
+	DOC "The directory where cublas_v2.h (and cuda_runtime.h) resides")
+  FIND_LIBRARY( CUDA_LIBRARY
+    NAMES cudart
+    PATHS
+	${CUDA_PATH}/lib/${CUBLAS_64_PATH}
+    DOC "The CUDA library")
+  FIND_LIBRARY( CUBLAS_LIBRARY
+    NAMES cublas
+    PATHS
+	${CUDA_PATH}/lib/${CUBLAS_64_PATH}
+    DOC "The CuBLAS library")
 ELSEIF(APPLE)
 #TODO
 ELSE (WIN32)
+  if(CUBLAS_64)
+    set(CUBLAS_64_SUFFIX "64")
+  else(CUBLAS_64)
+    set(CUBLAS_64_SUFFIX "")
+  endif(CUBLAS_64)
+  
   FIND_PATH( CUBLAS_INCLUDE_DIR cublas_v2.h
     /usr/local/cuda/include
     DOC "The directory where cublas_v2.h (and cuda_runtime.h) resides")
