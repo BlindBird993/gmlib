f0d205373502af6fa8493c0424b282ed4854de4f
CMN <panser_bjarne@hotmail.com>
2013-04-09 16:21:22 +0200

    Now uses ACML and MKL for DMatrix multiplication with float and double

---
+++ src/core/blas/acml/gmacmlmatmult.c
@@ -0,0 +1,99 @@
+/**********************************************************************************
+**
+** Copyright (C) 2013 Narvik University College
+** Contact: GMlib Online Portal at http://episteme.hin.no
+**
+** This file is part of the Geometric Modeling Library, GMlib.
+**
+** GMlib is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** GMlib is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with GMlib.  If not, see <http://www.gnu.org/licenses/>.
+**
+**********************************************************************************/
+
+
+
+/*! \file gmacmlmatmult.c
+ *
+ *  Implementation of ACML-specific matrix multiplication operators.
+ */
+
+#include <acml.h>
+
+namespace GMlib
+{
+
+inline
+const DMatrix<float>&  operator*(const DMatrix<float>& m, const DMatrix<float>& b)
+{
+	static const char transpose = 'T';
+	static const char noTranspose = 'N';
+	static DMatrix<float> r;
+	static Array<float> cA;
+	static Array<float> cB;
+	r.setDim(m.getDim1(), b.getDim2());
+	cA.setSize(m.getDim1() * m.getDim2());
+	cB.setSize(b.getDim1() * b.getDim2());
+	for(int i=0; i<cA.getSize(); i++)
+	{
+		cA[i] = m(i%m.getDim1())(i/m.getDim1());
+	}
+	for(int i=0; i<cB.getSize(); i++)
+	{
+		cB[i] = b(i%b.getDim1())(i/b.getDim1());
+	}
+
+	static Array<float> work;
+	work.setSize(m.getDim1() * b.getDim2());
+
+	sgemm(noTranspose, noTranspose, m.getDim1(), b.getDim2(), m.getDim2(), 1.0f, cA.getPtr(), m.getDim1(), cB.getPtr(), b.getDim1(), 0.0f, work.getPtr(), m.getDim1());
+
+	for(int i=0; i<r.getDim1(); i++)
+		for(int j=0; j<r.getDim2(); j++)
+			r[i][j]=(float) work[i+j*r.getDim1()];
+
+	return r;
+}
+
+inline
+const DMatrix<double>&  operator*(const DMatrix<double>& m, const DMatrix<double>& b)
+{
+	static const char transpose = 'T';
+	static const char noTranspose = 'N';
+	static DMatrix<double> r;
+	static Array<double> cA;
+	static Array<double> cB;
+	r.setDim(m.getDim1(), b.getDim2());
+	cA.setSize(m.getDim1() * m.getDim2());
+	cB.setSize(b.getDim1() * b.getDim2());
+	for(int i=0; i<cA.getSize(); i++)
+	{
+		cA[i] = m(i%m.getDim1())(i/m.getDim1());
+	}
+	for(int i=0; i<cB.getSize(); i++)
+	{
+		cB[i] = b(i%b.getDim1())(i/b.getDim1());
+	}
+
+	static Array<double> work;
+	work.setSize(m.getDim1() * b.getDim2());
+
+	dgemm(noTranspose, noTranspose, m.getDim1(), b.getDim2(), m.getDim2(), 1.0, cA.getPtr(), m.getDim1(), cB.getPtr(), b.getDim1(), 0.0, work.getPtr(), m.getDim1());
+
+	for(int i=0; i<r.getDim1(); i++)
+		for(int j=0; j<r.getDim2(); j++)
+			r[i][j]=(float) work[i+j*r.getDim1()];
+
+	return r;
+}
+
+} // namespace GMlib
