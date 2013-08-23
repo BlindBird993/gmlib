7ccbfd5a9ca123d70835b881adab215aaf9171d3
Jostein Bratlie <jostein.bratlie@gmail.com>
2013-08-23 13:21:12 +0200

    GLEW re-enabled.

--- src/opengl/gmopengl.h
+++ src/opengl/gmopengl.h
@@ -79,55 +79,55 @@
 #include <core/utils/gmstream.h>
 
 // Include Glew header and leave the gl mangling to GLEW
-//#include <GL/glew.h>
+#include <GL/glew.h>
 
-#if defined(_WIN32)
-//  To avoid including <windows.h> to avoid name space pollution,
-//	but Win32's <GL/gl.h> needs APIENTRY and WINGDIAPI defined properly.
-# if 1
-#  define  WIN32_LEAN_AND_MEAN
-#  include <windows.h>
-# else
-   // XXX This is from Win32's <windef.h>
-#  ifndef APIENTRY
-#   define GLUT_APIENTRY_DEFINED
-#   if (_MSC_VER >= 800) || defined(_STDCALL_SUPPORTED)
-#    define APIENTRY    __stdcall
-#   else
-#    define APIENTRY
-#   endif
-#  endif
-   // XXX This is from Win32's <winnt.h>
-#  ifndef CALLBACK
-#   if (defined(_M_MRX000) || defined(_M_IX86) || defined(_M_ALPHA) || defined(_M_PPC)) && !defined(MIDL_PASS)
-#    define CALLBACK __stdcall
-#   else
-#    define CALLBACK
-#   endif
-#  endif
-   // XXX This is from Win32's <wingdi.h> and <winnt.h>
-#  ifndef WINGDIAPI
-#   define GLUT_WINGDIAPI_DEFINED
-#   define WINGDIAPI __declspec(dllimport)
-#  endif
-   // XXX This is from Win32's <ctype.h>
-#  ifndef _WCHAR_T_DEFINED
-typedef unsigned short wchar_t;
-#   define _WCHAR_T_DEFINED
-#  endif
-# endif
-#endif
-
-
-#include <GL/gl.h>
+//#if defined(_WIN32)
+////  To avoid including <windows.h> to avoid name space pollution,
+////	but Win32's <GL/gl.h> needs APIENTRY and WINGDIAPI defined properly.
+//# if 1
+//#  define  WIN32_LEAN_AND_MEAN
+//#  include <windows.h>
+//# else
+//   // XXX This is from Win32's <windef.h>
+//#  ifndef APIENTRY
+//#   define GLUT_APIENTRY_DEFINED
+//#   if (_MSC_VER >= 800) || defined(_STDCALL_SUPPORTED)
+//#    define APIENTRY    __stdcall
+//#   else
+//#    define APIENTRY
+//#   endif
+//#  endif
+//   // XXX This is from Win32's <winnt.h>
+//#  ifndef CALLBACK
+//#   if (defined(_M_MRX000) || defined(_M_IX86) || defined(_M_ALPHA) || defined(_M_PPC)) && !defined(MIDL_PASS)
+//#    define CALLBACK __stdcall
+//#   else
+//#    define CALLBACK
+//#   endif
+//#  endif
+//   // XXX This is from Win32's <wingdi.h> and <winnt.h>
+//#  ifndef WINGDIAPI
+//#   define GLUT_WINGDIAPI_DEFINED
+//#   define WINGDIAPI __declspec(dllimport)
+//#  endif
+//   // XXX This is from Win32's <ctype.h>
+//#  ifndef _WCHAR_T_DEFINED
+//typedef unsigned short wchar_t;
+//#   define _WCHAR_T_DEFINED
+//#  endif
+//# endif
+//#endif
 
 
-#ifndef GL_GLEXT_VERSION
-# define GL_GLEXT_VERSION 86
-#endif
+//#include <GL/gl.h>
 
-#include <GL/glext.h>
-#include <GL/glu.h>
+
+//#ifndef GL_GLEXT_VERSION
+//# define GL_GLEXT_VERSION 86
+//#endif
+
+//#include <GL/glext.h>
+//#include <GL/glu.h>
 
 // local
 #include "utils/gmutils.h"
